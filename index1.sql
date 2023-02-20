2ab9bb83f8304b06b4be30fec9f8f2938ff30a4d
vkand <vvkand@mail.ru>
2023-02-20 19:51:32 +0700

    ДЗ

---
+++ index1.sql
@@ -0,0 +1,13 @@
+-- create
+CREATE TABLE EMPLOYEE (
+id INTEGER PRIMARY KEY,
+name TEXT NOT NULL,
+age TEXT NOT NULL,
+address TEXT NOT NULL
+);
+-- insert
+INSERT INTO EMPLOYEE VALUES (0001, 'Гущин', '30', 'Москва');
+INSERT INTO EMPLOYEE VALUES (0002, 'Лукашков', '29', 'Новосибирск');
+INSERT INTO EMPLOYEE VALUES (0003, 'Васильев', '35', 'Бердск');
+INSERT INTO EMPLOYEE VALUES (0004, 'Судоров', '45', 'Искитим');
+INSERT INTO EMPLOYEE VALUES (0005, 'Мальцев', '40', 'Бердск');
